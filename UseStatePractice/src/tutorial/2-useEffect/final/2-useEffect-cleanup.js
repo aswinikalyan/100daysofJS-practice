0ded8e83ff5d17766178984202b109bf684bbe7e
aswinikalyan30 <76677408+aswinikalyan30@users.noreply.github.com>
2022-04-17 09:53:13 +0530

    UseEffect basics

---
+++ UseStatePractice/src/tutorial/2-useEffect/final/2-useEffect-cleanup.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+
+// cleanup function
+// second argument
+
+const UseEffectCleanup = () => {
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    console.log('useEffect');
+    window.addEventListener('resize', checkSize);
+    return () => {
+      console.log('cleanup');
+      window.removeEventListener('resize', checkSize);
+    };
+  }, []); //the [] indicates that the component only renders once
+  //when you have a parameter inside the [], then it is called whenver that parameter is changed,the function is called
+  console.log('render');
+  return (
+    <>
+      <h1>window</h1>
+      <h2>{size} PX</h2>
+    </>
+  );
+};
+
+export default UseEffectCleanup;
